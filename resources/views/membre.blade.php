3cd16283669c1c60f341955d904548ecd162a587
Mahfoud <hanimahfoud@gmail.com>
2022-10-25 08:07:32 +0200

    "next edit"

--- resources/views/membre.blade.php
+++ resources/views/membre.blade.php
@@ -21,7 +21,6 @@
   
   <!-- New member form -->
   <h2>Ajouter un(e) Argonaute</h2>
-
   <form action="" method="POST" enctype="multipart/form-data" class="new-member-form">
   @csrf
   <label for="nombre">Nombre</label>
